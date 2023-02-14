0e851d9738b46d6563f34985729a0feecc830ffd
sva <vandaele.seba@gmail.com>
2023-02-14 20:20:07 +0100

    adding dependency injection

---
+++ Src/messaging-center/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using messaging_center.Impl;
+using messaging_center.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace messaging_center
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddMessagingCenter(this IServiceCollection services)
+        {
+            services.AddSingleton<IMessagingCenter, MessagingCenter>();
+            return services;
+        }
+    }
+}
